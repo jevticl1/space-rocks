0f1b1c4eaf9f973953ef4642f782fa0e97c16ce7
jevticl1 <jevticl1@udayton.edu>
2024-10-09 10:28:04 -0400

    Changed player movement to be influenced by forces acting upon ship instead of directly changing position or velocity each frame.

--- player/player.gd
+++ player/player.gd
@@ -5,12 +5,14 @@ extends RigidBody2D
 
 var thrust = Vector2.ZERO
 var rotation_dir = 0
+var screensize = Vector2.ZERO
 
 enum {INIT, ALIVE, INVULNERABLE, DEAD}
 var state = INIT
 
 func _ready():
 	change_state(ALIVE)
+	screensize = get_viewport_rect().size
 	
 func _process(delta):
 	get_input()
@@ -19,13 +21,12 @@ func _physics_process(delta):
 	constant_force = thrust
 	constant_torque = rotation_dir * spin_power
 
-func get_input():
-	thrust = Vector2.ZERO
-	if state in [DEAD, INIT]:
-		return
-	if Input.is_action_pressed("thrust"):
-		thrust = transform.x * engine_power
-	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+func _integrate_forces(physics_state):
+	var xform = physics_state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
+	physics_state.transform = xform
+	
 
 func change_state(new_state):
 	match new_state:
@@ -38,4 +39,11 @@ func change_state(new_state):
 		DEAD:
 			$CollisionShape2D.set_deferred("disabled", true)
 	state = new_state
-	
+
+func get_input():
+	thrust = Vector2.ZERO
+	if state in [DEAD, INIT]:
+		return
+	if Input.is_action_pressed("thrust"):
+		thrust = transform.x * engine_power
+	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
