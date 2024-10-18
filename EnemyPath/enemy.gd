6449b7ff02e2e818d1c08569ad6829b4889b506e
jevticl1 <jevticl1@udayton.edu>
2024-10-18 10:46:46 -0400

    Enemies now target and shoot the player in 3 bullet bursts

--- EnemyPath/enemy.gd
+++ EnemyPath/enemy.gd
@@ -4,9 +4,38 @@ extends Area2D
 @export var speed = 150
 @export var rotation_speed = 120
 @export var health = 3
+@export var bullet_spread = 0.2
 
 var follow = PathFollow2D.new()
 var target = null
+
+func shoot():
+		var dir = global_position.direction_to(target.global_position)
+		dir = dir.rotated(randf_range(-bullet_spread, bullet_spread))
+		var b = bullet_scene.instantiate()
+		get_tree().root.add_child(b)
+		b.start(global_position, dir)
+
+func shoot_pulse(n, delay):
+	for i in n:
+		shoot()
+		await get_tree().create_timer(delay).timeout
+
+func take_damage(amount):
+	health -= amount
+	$AnimationPlayer.play("flash")
+	if health <= 0:
+		explode()
+
+func explode():
+	speed = 0
+	$GunCooldown.stop()
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sprite2D.hide()
+	$Explosion.show()
+	$Explosion/AnimationPlayer.play("Explosion")
+	await $Explosion/AnimationPlayer.animation_finished
+	queue_free()
 
 func _ready():
 	$Sprite2D.frame = randi() % 3
@@ -23,4 +52,9 @@ func _physics_process(delta: float):
 		queue_free()
 
 func _on_gun_cooldown_timeout() -> void:
-	pass # Replace with function body.
+	shoot_pulse(3, 0.15)
+
+func _on_body_entered(body) -> void:
+	if body.is_in_group("rocks"):
+		return
+	explode()
